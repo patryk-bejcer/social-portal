b65175bdd12e4dc004145dc0a1a66cdd3c69a022
Patryk <patryk.bejcer@gmail.com>
2018-01-21 21:00:22 +0100

    Email notifications and post comment and accepted friend notification to DB.

---
+++ app/Notifications/FriendRequestAccepted.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Support\Facades\Auth;
+
+class FriendRequestAccepted extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['database'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->line('The introduction to the notification.')
+                    ->action('Notification Action', url('/'))
+                    ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            'message' => 'Użytkownik <a href="' . url('/users/' . Auth::id()) . '">' . Auth::user()->name . '</a> przyjął Twoje zaproszenie do znajomych',
+        ];
+    }
+}
---
+++ app/Notifications/PostCommented.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Support\Facades\Auth;
+
+class PostCommented extends Notification
+{
+    use Queueable;
+
+    protected $post_id;
+    protected $comment_id;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($post_id, $comment_id)
+    {
+        $this->post_id = $post_id;
+        $this->comment_id = $comment_id;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['database'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->line('The introduction to the notification.')
+                    ->action('Notification Action', url('/'))
+                    ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            'message' => 'Użytkownik <a href="' . url('/users/' . Auth::id()) . '">' . Auth::user()->name . '</a> skomentował Twój 
+                <a href="'. url('/posts/' . $this->post_id ) . '">post</a> <br/> Kliknij <a href="'.url('/posts/' . $this->post_id . '/#comment_' . $this->comment_id ) . '">tutaj</a>
+                aby zobaczyć komentarz',
+        ];
+    }
+}
